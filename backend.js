1fcc57f9e99e152f7205b35875c9462d7bf817d7
Atri Thakar <atrithaker122@gmail.com>
2023-11-02 02:59:00 +0530

    Bug fixes

--- backend.js
+++ backend.js
@@ -3,41 +3,50 @@ const express = require("express");
 const app = express();
 const path = require("node:path");
 
+let currentUser = {
+  email: "",
+  password: "",
+};
+
 let applicationStatus = {
-    status: "Applied Successfully",
-    link: '<a href="../index.html">Go to Home</a>',
+  status: "Applied Successfully",
+  link: '<a href="../index.html">Go to Home</a>',
 };
 
 mongoose.connect("mongodb://127.0.0.1:27017/project");
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function () {
-    console.log("Connection Successful");
+  console.log("Connection Successful");
 });
 var userresp = new mongoose.Schema({
-    email: String,
-    password: String,
+  email: String,
+  password: String,
 });
 var user = mongoose.model("user", userresp);
 
 var subscriberresp = new mongoose.Schema({
-    email: String,
+  email: String,
 });
 var subscriber = mongoose.model("subscriber", subscriberresp);
 
 var jobseekerresp = new mongoose.Schema({
-    keywords: String,
-    location: String,
-    jobrole: String,
+  email: {
+    type: String,
+    default: currentUser.email,
+  },
+  keywords: String,
+  location: String,
+  jobrole: String,
 });
 var jobseeker = mongoose.model("jobseeker", jobseekerresp);
 
 var applicationresp = new mongoose.Schema({
-    name: String,
-    age: Number,
-    qualification: String,
-    degree: String,
-    resume: String,
+  name: String,
+  age: Number,
+  qualification: String,
+  degree: String,
+  resume: String,
 });
 var application = mongoose.model("application", applicationresp);
 
@@ -45,121 +54,131 @@ const hostname = "127.0.0.1";
 const port = 3000;
 
 app.get("/", (req, res) => {
-    let p = path.join(__dirname, "public", "html","login.html");
-    res.sendFile(p);
+  let p = path.join(__dirname, "public", "html", "login.html");
+  res.sendFile(p);
 });
 app.get("/signup", (req, res) => {
-    let p = path.join(__dirname, "public", "html","signup2.html");
-    res.sendFile(p);
+  let p = path.join(__dirname, "public", "html", "signup2.html");
+  res.sendFile(p);
 });
-app.use(express.static("public")); 
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 // console.log(__dirname)
 
-
 app.get("/apply", (req, res) => {
-    let p = path.join(__dirname, "public", "html");
-    res.sendFile(p + "/jobApplication.html");
+  let p = path.join(__dirname, "public", "html");
+  res.sendFile(p + "/jobApplication.html");
 });
 
 app.post("/", (req, res) => {
-    let p = path.join(__dirname, "public");
-    res.sendFile(p + "/index.html");
+  let p = path.join(__dirname, "public");
+  res.sendFile(p + "/index.html");
 });
 
 let findJobStatus = {};
 app.post("/findjob", (req, res) => {
-    let mydata = new jobseeker(req.body);
-    mydata
-        .save()
-        .then(() => {
-            findJobStatus = {
-                status: "Your application has been received",
-            };
-            res.send("ok");
-        })
-        .catch(() => {
-            res.send("not ok");
-        });
+  let mydata = new jobseeker(req.body);
+  mydata.email = currentUser.email;
+  mydata
+    .save()
+    .then(async () => {
+      // let userq = user.findOne({email:currentUser.email,password:currentUser.password})
+      // console.log(userq);
+      // await jobseeker.updateOne({email:''},{email: userq.email})
+      // console.log(user)
+      // jobseeker.email.push(currentUser.email)
+
+      findJobStatus = {
+        status: "Your application has been received",
+      };
+      res.send("ok");
+    })
+    .catch(() => {
+      res.send("not ok");
+    });
 });
 
 var subscribeStatus = {};
 app.post("/subscribe", (req, res) => {
-    let mydata = new subscriber(req.body);
-    mydata
-        .save()
-        .then(() => {
-            subscribeStatus = {
-                status: "Subscribed!",
-            };
-            let p = path.join(__dirname, "public");
-            res.sendFile(p + "/index.html");
-        })
-        .catch(() => {
-            subscribeStatus = {
-                status: "Oops! Something went wrong. Please try again.",
-            };
-            let p = path.join(__dirname, "public");
-            res.sendFile(p + "/index.html");
-        });
+  let mydata = new subscriber(req.body);
+  mydata
+    .save()
+    .then(() => {
+      subscribeStatus = {
+        status: "Subscribed!",
+      };
+      let p = path.join(__dirname, "public");
+      res.sendFile(p + "/index.html");
+    })
+    .catch(() => {
+      subscribeStatus = {
+        status: "Oops! Something went wrong. Please try again.",
+      };
+      let p = path.join(__dirname, "public");
+      res.sendFile(p + "/index.html");
+    });
 });
 app.post("/application", (req, res) => {
-    let mydata = new application(req.body);
-    mydata
-        .save()
-        .then(() => {
-            let p = path.join(__dirname, "public", "html");
-            res.sendFile(p + "/jobApplication.html");
-        })
-        .catch();
+  let mydata = new application(req.body);
+  mydata
+    .save()
+    .then(() => {
+      let p = path.join(__dirname, "public", "html");
+      res.sendFile(p + "/jobApplication.html");
+    })
+    .catch();
 });
 app.get("/api", (req, res) => {
-    res.json(applicationStatus);
-    // applicationStatus ={}
+  res.json(applicationStatus);
+  // applicationStatus ={}
 });
 
 app.get("/api/subscribed", (req, res) => {
-    res.json(subscribeStatus);
-    // console.log(subscribeStatus.status)
+  res.json(subscribeStatus);
+  subscribeStatus.status=undefined
+  // console.log(subscribeStatus.status)
 });
 app.get("/api/signup", (req, res) => {
-    res.json(signupStatus);
-    // console.log(subscribeStatus.status)
+  res.json(signupStatus);
+  // console.log(subscribeStatus.status)
 });
 let signupStatus = {};
 app.post("/signup", (req, res) => {
-    let mydata = new user(req.body);
-    mydata
-        .save()
-        .then(() => {
-            let p = path.join(__dirname, "public", "html");
-            res.sendFile(p + "/login.html");
-        })
-        .catch(() => {
-            signupStatus = {
-                status: "Oops! Something went wrong!! Please try again later!!!",
-            };
-            res.json(signupStatus);
-        });
+  let mydata = new user(req.body);
+  mydata
+    .save()
+    .then(() => {
+      let p = path.join(__dirname, "public", "html");
+      res.sendFile(p + "/login.html");
+    })
+    .catch(() => {
+      signupStatus = {
+        status: "Oops! Something went wrong!! Please try again later!!!",
+      };
+      res.json(signupStatus);
+    });
 });
 
 app.post("/login", async (req, res) => {
-    try {
-        let check = await user.findOne({ email: req.body.email });
-        if (check.password === req.body.password) {
-            let p = path.join(__dirname, "public");
-            res.sendFile(p + "/" + "index.html");
-        } else {
-            let p = path.join(__dirname, "public", "html");
-            res.sendFile(p + "/" + "login.html");
-        }
-    } catch {
-        let p = path.join(__dirname, "public", "html");
-        res.sendFile(p + "/" + "login.html");
+  try {
+    let check = await user.findOne({ email: req.body.email });
+    if (check.password === req.body.password) {
+      let p = path.join(__dirname, "public");
+      currentUser.email = req.body.email;
+      currentUser.password = req.body.password;
+      // console.log(currentUser)
+      res.sendFile(p + "/" + "index.html");
+    } else {
+      let p = path.join(__dirname, "public", "html");
+      res.sendFile(p + "/" + "login.html");
     }
+  } catch {
+    let p = path.join(__dirname, "public", "html");
+    res.sendFile(p + "/" + "login.html");
+  }
 });
 
 app.listen(port, hostname, () => {
-    // console.log(`Server running at http://${hostname}:3000/`);
-    console.log(`Server running at http://${hostname}:${port}/`);
+  // console.log(`Server running at http://${hostname}:3000/`);
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
